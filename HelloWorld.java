88db2af7522f7060cb3691a06af27fa061853519
zoudj113 <zoudj113@foxmail.com>
2026-07-10 17:31:14 +0800

    ttt

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-		String str3 = null;
-		System.out.println(str3.toString());
+		String str4 = null;
+		System.out.println(str4.toString());
         System.out.println("hello world");
     }
 }
